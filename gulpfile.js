8095650debdf08b748d9911f0215531be404d914
Ilya Loginov <isloginov@gmail.com>
2016-10-09 12:52:31 +0300

    Add demon gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
+'use strict';
+
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
+var nodemon = require('gulp-nodemon');
 var tsProject = ts.createProject('server/tsconfig.json');
 
-gulp.task('default', function () {
+gulp.task('compile', function() {
     return tsProject.src()
         .pipe(tsProject()).js
         .pipe(gulp.dest('dist'));
-})
+});
 
-gulp.task('watch', ['default'], function() {
-    gulp.watch('server/**/*.ts', ['default']);
+gulp.task('watch', ['compile'], function() {
+    gulp.watch('server/**/*.ts', ['compile'])
+});
+
+gulp.task('demon', ['watch'], function() {
+    nodemon({
+        script: 'dist/main.js',
+    })
 });
